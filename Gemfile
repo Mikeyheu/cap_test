cd269f8df02b07153f7e3747644976a8f907176e
Michael Heu <mheu@clickbooq.com>
2014-03-10 20:41:16 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :development do
+	gem "capistrano", "~> 2.15.5"
+end
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+# Use unicorn as the app server
+gem 'unicorn'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CapTest::Application.config.secret_key_base = '10ac787c990da6b2858cb4a673126c450520e50059f92478bdc5f30973568d394d8554f7d37f5d637943d2911077998d1c37dc03355aee89d33e9f84b6751ea7'
